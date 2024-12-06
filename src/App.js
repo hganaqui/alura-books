5015cb8658c7d3ad1187c80a0cc53e0ae7c11490
Henrique Ganaqui <henrique.ganaqui@DASA.COM.BR>
2024-12-06 17:01:57 -0300

    Subida app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import './App.css';
 import Logo from './componentes/Logo'
+import perfil from './images/perfil.svg'
+import sacola from './images/sacola.svg'
+
+
+const textoOpcoes = ["CATEGORIAS", "FAVORITOS", "Minha ESTANTE"]
+const icones = [perfil, sacola]
 
 function App() {
   return (
     <div className="App">
       <header className='App-header'>
         <Logo></Logo>
+        <ul className='opcoes'>
+          {
+            textoOpcoes.map((texto) => (
+              <li className='opcao'><p>{texto}</p></li>
+            ))
+          }
+        </ul>
+
+        <ul className='icones'>
+          {icones.map((icone) =>
+            <li className='icone'><img src={icone} alt="icone"></img></li>
+          )}
+        </ul>
       </header>
     </div>
   );
