6499a9056381fbc79c2f3d9840b2766c3fee67ca
Henrique Ganaqui <henrique.ganaqui@DASA.COM.BR>
2024-12-06 16:48:59 -0300

    Correções

--- src/componentes/Logo/index.js
+++ src/componentes/Logo/index.js
@@ -1,9 +1,14 @@
 import logo from '../../images/logo.svg';
+import './estilo.css';
+
 
 function Logo() {
     return (
         <div className='logo'>
-            <img src={logo} alt="Logo da empresa Alura Books, é um triangulo"></img>
+            <img
+                src={logo}
+                alt="Logo da empresa Alura Books, é um triangulo" 
+                className='logo-img'></img>
             <p><strong>Alura</strong> Books</p>
         </div>
     );
