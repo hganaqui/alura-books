38fff0b43c57119369349d710ceb8f676f076656
Henrique Ganaqui <henrique.ganaqui@DASA.COM.BR>
2024-12-09 14:58:22 -0300

    subida logoimg

--- src/componentes/Logo/index.js
+++ src/componentes/Logo/index.js
@@ -1,22 +1,22 @@
 import styled from 'styled-components';
 import logo from '../../images/logo.svg';
 
-const LogoContainer = styled.div `
+const LogoContainer = styled.div`
     display: flex;
     font-size: 30px; 
+`
 
-.logo-img{
-    margin-right: 10px;
-}
+const LogoImageContainer = styled.img`
+ margin-right: 10px;
 `
 
 function Logo() {
     return (
         <LogoContainer>
-            <img
+            <LogoImageContainer
                 src={logo}
-                alt="Logo da empresa Alura Books, é um triangulo" 
-                className='logo-img'></img>
+                alt="Logo da empresa Alura Books, é um triangulo"
+            />
             <p><strong>Alura</strong> Books</p>
         </LogoContainer>
     );
