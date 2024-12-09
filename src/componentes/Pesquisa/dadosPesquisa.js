b21adb3ad3a837038de8b0e3f62e36f4686d4de7
Henrique Ganaqui <henrique.ganaqui@DASA.COM.BR>
2024-12-09 16:47:11 -0300

    Subida estados

---
+++ src/componentes/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,8 @@
+import livro from '../../images/livro.png'
+
+export const livros = [
+    { nome: 'Liderança em Design', id: 1, src: livro },
+    { nome: 'Design', id: 2, src: livro },
+    { nome: 'Java Script', id: 3, src: livro },
+    { nome: 'Em', id: 4, src: livro },
+]
